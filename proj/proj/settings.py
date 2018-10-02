c77926c0094def1defe929b620fd480c9083105e
Sanjay Saraf <svsaraf90@gmail.com>
2018-10-01 22:28:40 -0400

    added static root

--- proj/proj/settings.py
+++ proj/proj/settings.py
@@ -117,6 +117,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+STATIC_ROOT = u'/home/svsaraf/solovine/static'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
